26f2cac91be4f276f6e94dfafabacfd083ef2900
renegade17 <username@users.noreply.github.com>
2017-09-21 15:27:14 -0400

    Modified vector code

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,32 @@
  #include "Football.h"
- #include "Football.cpp"
  #include <vector>
+ #include <string>
  
  int main ()
  {
 	 string name;
 	 int goals;
-	 vector <Football> Leaguecup (4);
+	 vector <Football> Leaguecup;
 	 
-	 for (int i=0;i<Leaguecup.size();i++)
+	 for (int i=0;i<4;i++)
 {
-	 cout<<"Enter your team name";
-	 getline(cin, name);
-	 cout<<"Enter goals scored";
+	 cout<<"Enter your team name "<<endl;
+	 cin>>name;
+	 cout<<"Enter goals scored "<<endl;
 	 cin>>goals;
+	 
+	 Football Tourney(name, goals);
+	 Leaguecup.push_back ({name, goals});
  }
+  for (auto v:Leaguecup)
+  {
+	  cout<<v.getTeamName()<<" "<<endl;
+	  cout<<v.getGoals()<<endl;
+  }
+  cout<<endl;
+}
+
+ 
  
  
 	 
